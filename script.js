18eb60da208780f12608570e6b43e1fa23921caa
Carlosvva <cv.1411.vargas@gmil.com>
2016-08-20 11:45:52 -0600

    Tu pinche pancreas :v

---
+++ script.js
@@ -0,0 +1,8 @@
+var myApp= angular.module("myApp",[])
+myApp.controller('mainCtrl',function($scope){
+  $scope.var1="tu pinche pancreas";
+  $scope.var2="tengo hambre";
+  $scope.var3="ya quiero comer";
+  $scope.var4="lechuga";
+  $scope.names =['Igor', 'Misko', 'Vojta'];
+})
